260bd18bca7e19dd45a8b878fbf6ed7eadd88b57
Aditya Panchal <adityapanchal1946198@gmail.com>
2023-03-31 18:12:41 +0530

    first commit

---
+++ blockchain/truffle-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+      gas: 6721975,
+      from: '0xb58ac816baAF24E922AE4611777ddC2cb59613A6',
+    },
+  },
+  contracts_directory: './testing/',
+  contracts_build_directory: './src/build/',
+  compilers: {
+    solc: {
+      version: "0.6.6",
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 100
+        }
+      }
+    }
+  }
+}
